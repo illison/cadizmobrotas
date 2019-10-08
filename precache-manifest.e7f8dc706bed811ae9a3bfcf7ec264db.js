a113337fd85ed56c6682513a52b877da420cd4f3
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2019-10-08 16:02:18 -0300

    Updates

---
+++ precache-manifest.e7f8dc706bed811ae9a3bfcf7ec264db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d7be1813728ab46826e9d91d80f84c",
+    "url": "/teste/index.html"
+  },
+  {
+    "revision": "1ef4390b4c8fb3842638",
+    "url": "/teste/static/css/main.2385ed7d.chunk.css"
+  },
+  {
+    "revision": "0e2caf127f20b2cde528",
+    "url": "/teste/static/js/2.c4b8f3ac.chunk.js"
+  },
+  {
+    "revision": "1ef4390b4c8fb3842638",
+    "url": "/teste/static/js/main.b4cf260f.chunk.js"
+  },
+  {
+    "revision": "92c669ff284ac44f807e",
+    "url": "/teste/static/js/runtime-main.0849b3df.js"
+  }
+]);
